338de83bc957291fda094a747910cabf84536096
Kilian Evang <noreply@texttheater.net>
2017-08-20 10:19:46 +0200

    Changed directory structure
    To allow for different user versions of output files

--- ccgweb/__init__.py
+++ ccgweb/__init__.py
@@ -7,16 +7,17 @@ import subprocess
 class Sentence:
 
     def on_get(self, req, res, sentence):
-        # TODO refuse sentences not in our database
         sentence_hash = hashlib.sha1(sentence.encode('UTF-8')).hexdigest()
-        dirname = os.path.join('out', sentence_hash[:2])
-        rawname = os.path.join(dirname, sentence_hash + '.raw')
-        if not os.path.isfile(rawname):
-            ccgweb.util.makedirs(dirname)
-            with open(rawname, 'w', encoding='UTF-8') as f:
+        hash_group = sentence_hash[:2]
+        raw_dir = os.path.join('raw', hash_group)
+        raw_file = os.path.join(raw_dir, sentence_hash + '.raw')
+        out_dir = os.path.join('out', hash_group, sentence_hash)
+        der_file = os.path.join(out_dir, 'auto.der.xml')
+        if not os.path.isfile(raw_file):
+            ccgweb.util.makedirs(raw_dir)
+            with open(raw_file, 'w', encoding='UTF-8') as f:
                 f.write(sentence)
-        dername = os.path.join(dirname, sentence_hash + '.der.xml')
-        subprocess.check_call(('./ext/produce/produce', dername))
+        subprocess.check_call(('./ext/produce/produce', der_file))
         res.content_type = 'application/xml'
-        with open(dername, 'rb') as f:
+        with open(der_file, 'rb') as f:
             res.data = f.read()
