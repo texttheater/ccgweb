3d5d20efa9124a40b3f701a2de75de9a8364b0d8
Kilian Evang <noreply@texttheater.net>
2017-09-06 13:41:39 +0200

    Simplified WSGI app module

--- ccgweb/app.py
+++ ccgweb/app.py
@@ -2,11 +2,5 @@ import ccgweb
 import falcon
 
 
-def create_app():
-    api = falcon.API()
-    api.add_route('/sentences/{sentence}', ccgweb.Sentence())
-    return api
-
-
-def get_app():
-    return create_app()
+application = falcon.API()
+application.add_route('/sentences/{sentence}', ccgweb.Sentence())
