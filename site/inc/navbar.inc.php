9ead12599b77c335582bd019046bbd34f5a45096
Kilian Evang <noreply@texttheater.net>
2017-10-30 20:24:03 +0100

    Some style changes

--- site/inc/navbar.inc.php
+++ site/inc/navbar.inc.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default navbar-static-top">
+<nav class="navbar navbar-inverse navbar-static-top">
 	<div class="container-fluid">
 		<div class="navbar-header">
 			<a class="navbar-brand" href="#">CCGWeb</a>
