a4c35c03a64a7a6e29abe8828132c1bff888cc0d
Kilian Evang <noreply@texttheater.net>
2017-11-08 17:55:44 +0100

    Compatibility with older Falcon versions

--- src/python/ccgweb/app.py
+++ src/python/ccgweb/app.py
@@ -3,7 +3,10 @@ import falcon
 
 
 application = falcon.API()
-application.req_options.auto_parse_form_urlencoded = True
+
+if hasattr(application.req_options, 'auto_parse_form_urlencoded'):
+    application.req_options.auto_parse_form_urlencoded = True
+
 application.add_route('/sentences/{sentence}/{user_id}', ccgweb.Sentence())
 application.add_route('/login', ccgweb.Login())
 application.add_route('/session', ccgweb.Session())
