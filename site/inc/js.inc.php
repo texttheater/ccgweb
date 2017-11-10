fcfb2af590fd608b887e484dbd0a18cd51918154
Kilian Evang <noreply@texttheater.net>
2017-11-10 22:23:34 +0100

    Removed jQuery UI
    Looks like we are not going to use it for span BOWs.

--- site/inc/js.inc.php
+++ site/inc/js.inc.php
@@ -1,10 +1,6 @@
 <!-- jQuery -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-<!-- jQuery UI -->
-<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-
 <!-- Bootstrap scripts -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
