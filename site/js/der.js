0b0bdb00cfadb55b87c867c04d997e9d83ec5477
Kilian Evang <noreply@texttheater.net>
2017-11-10 15:14:54 +0100

    Removed a check that is no longer necessary
    The elements selected for here do not exist if the user is not logged
    in.

--- site/js/der.js
+++ site/js/der.js
@@ -1,41 +1,39 @@
-if (isUserLoggedIn) {
-    document.querySelectorAll('div#parses_mine table.lex').forEach(table => {
-        const td = table.querySelector('td.cat')
-        td.onfocus = event => {
-            if (td.firstChild.nodeType == Node.TEXT_NODE) {
-                const currentCat = td.textContent.trim()
-                const input = document.createElement('input')
-                input.type = 'text'
-                input.value = currentCat
-                td.removeChild(td.firstChild)
-                td.appendChild(input)
-                input.focus()
-                input.onblur = event => {
-                    let newCat = input.value.trim()
+document.querySelectorAll('div#parses_mine table.lex').forEach(table => {
+    const td = table.querySelector('td.cat')
+    td.onfocus = event => {
+        if (td.firstChild.nodeType == Node.TEXT_NODE) {
+            const currentCat = td.textContent.trim()
+            const input = document.createElement('input')
+            input.type = 'text'
+            input.value = currentCat
+            td.removeChild(td.firstChild)
+            td.appendChild(input)
+            input.focus()
+            input.onblur = event => {
+                let newCat = input.value.trim()
 
-                    if (newCat == '') {
-                        newCat = '\u00A0' // non-breaking space
+                if (newCat == '') {
+                    newCat = '\u00A0' // non-breaking space
+                }
+
+                const textNode = document.createTextNode(newCat)
+                td.removeChild(input)
+                td.appendChild(textNode)
+            }
+            input.onchange = event => {
+                api(
+                    'sentences/' + encodeURIComponent(sentence) + '/' + encodeURIComponent(userName),
+                    'add_super_bow',
+                    {
+                        offset_from: table.dataset['from'],
+                        offset_to: table.dataset['to'],
+                        tag: input.value
+                    },
+                    function() {
+                        console.log(this.responseText)
                     }
-
-                    const textNode = document.createTextNode(newCat)
-                    td.removeChild(input)
-                    td.appendChild(textNode)
-                }
-                input.onchange = event => {
-                    api(
-                        'sentences/' + encodeURIComponent(sentence) + '/' + encodeURIComponent(userName),
-                        'add_super_bow',
-                        {
-                            offset_from: table.dataset['from'],
-                            offset_to: table.dataset['to'],
-                            tag: input.value
-                        },
-                        function() {
-                            console.log(this.responseText)
-                        }
-                    )
-                }
+                )
             }
         }
-    })
-}
+    }
+})
