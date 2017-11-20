79580b9bacc8657083b50ba2850f1f6ed181c06a
Kilian Evang <noreply@texttheater.net>
2017-11-20 14:09:50 +0100

    Refactored assignments module

--- src/python/ccgweb/assignments.py
+++ src/python/ccgweb/assignments.py
@@ -5,15 +5,22 @@ import json
 class Assignment:
 
     def on_get(self, req, res):
-        rows = ccgweb.db.get('''SELECT s.lang AS lang,
-            s.sentence AS sentence,
-            c.time IS NOT NULL AS done
-            FROM sentences AS s
-            LEFT OUTER JOIN correct AS c
-            ON s.lang = c.lang
-            AND s.sentence_id = c.sentence_id
-            WHERE s.assigned > 0''')
+        user = ccgweb.users.current_user(req)
+        if not user:
+            user = 'auto'
         body = [ { 'lang': lang, 'sentence': sentence, 'done': done }
-                 for lang, sentence, done in rows ]
+                 for lang, sentence, done in get_assignment(user) ]
         res.content_type = 'application/json'
         res.body = json.dumps(body)
+
+
+def get_assignment(user):
+    return ccgweb.db.get('''SELECT s.lang AS lang,
+        s.sentence AS sentence,
+        c.time IS NOT NULL AS done
+        FROM sentences AS s
+        LEFT OUTER JOIN correct AS c
+        ON s.lang = c.lang
+        AND s.sentence_id = c.sentence_id
+        AND c.user_id = %s
+        WHERE s.assigned > 0''', user)
