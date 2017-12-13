aafa324528fa2fa08d90e7ac31f92c297a801f61
Kilian Evang <noreply@texttheater.net>
2017-12-13 11:18:54 +0100

    Beefed up test webapp

--- src/python/ccgweb/testapp.py
+++ src/python/ccgweb/testapp.py
@@ -3,7 +3,7 @@ import falcon
 class String:
 
     def on_get(self, req, res, string):
-        print(string)
+        print(string, req.params['string'])
 
 application = falcon.API()
 application.add_route('/{string}', String())
