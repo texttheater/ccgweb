0f7857d35b88f73cad34d69755f601a0929c3450
Kilian Evang <noreply@texttheater.net>
2017-12-14 10:51:37 +0100

    Added a 1-sentence annotation manual

--- site/about.php
+++ site/about.php
@@ -14,8 +14,9 @@ require('inc/head.inc.php');
 
 <h2>About</h2>
 
-<p>CCGWeb is an annotation platform for Combinatory Categorial Grammar. It is
-currently in closed beta.</p>
+<p>CCGWeb is an annotation platform for Combinatory Categorial Grammar. The
+goal is to create a multilingual corpus of sentences annotated in the style of
+<a href=https://dl.acm.org/citation.cfm?id=1858703>CCGrebank</a>.</p>
 
 <p>All sentences are <a href=https://creativecommons.org/licenses/by/2.0/>CC-BY</a>
 <a href=https://tatoeba.org>Tatoeba</a>.</p>
