9307825458023018dda3db0f6208e85f7699ca9b
Kilian Evang <noreply@texttheater.net>
2018-01-23 13:52:34 +0100

    Less defensive wording

--- site/manual.php
+++ site/manual.php
@@ -20,7 +20,7 @@ derivation projection algorithms and cross-lingually trained parsers.</p>
 <p>We opt here for a flavor of CCG closely following CCGrebank. The reason for
 this choice is that a large volume of training data in this format (for
 English) is available. Having test data (in multiple languages) in a similar
-format will enable a more direct comparison.</p>
+format will enable a direct comparison.</p>
 
 <p>Of course, the annotation format for CCGrebank has only been specified for
 English. It is not always obvious how to apply it to other languages. This
