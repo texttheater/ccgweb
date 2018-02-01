a0f5d0ee40c2f00b07eb38bee87ed5eca81e03fe
Kilian Evang <noreply@texttheater.net>
2018-02-01 10:41:05 +0100

    Code formatting

--- site/inc/navbar.inc.php
+++ site/inc/navbar.inc.php
@@ -21,14 +21,12 @@
 		</ul>
 		<ul class="nav navbar-nav navbar-right">
 			<li>
-<?php
-if (basename($_SERVER['SCRIPT_FILENAME']) == 'user.php') {
-} elseif ($is_user_logged_in) {
-	echo '<a href=user.php><span class="glyphicon glyphicon-user" aria-hidden="true"></span> ' . htmlspecialchars($user_name) . "</a>\n";
-} else {
-	echo "<a href=user.php>Sign in</a>\n";
-}
-?>
+<?php if (basename($_SERVER['SCRIPT_FILENAME']) == 'user.php') { ?>
+<?php } elseif ($is_user_logged_in) { ?>
+				<a href=user.php><span class="glyphicon glyphicon-user" aria-hidden="true"></span> <?= htmlspecialchars($user_name) ?></a>
+<?php } else { ?>
+				<a href=user.php>Sign in</a>
+<?php } ?>
 			</li>
 		</ul>
 	</div>
