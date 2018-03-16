1b7c5c7ec6171f8101d8087f657df30dce0e1ffa
Kilian Evang <noreply@texttheater.net>
2018-03-16 23:07:28 +0100

    Slight layout improvement

--- site/manual.php
+++ site/manual.php
@@ -36,23 +36,27 @@ require('inc/head.inc.php');
 
 <div class=container>
 	<h2>Manual</h2>
-	<div style="height: 20px;"></div>
-	<div class=row>
-		<div class=col-md-3>
-			<div class=list-group>
-
-<?php foreach ($sections as $s => $t) { ?>
-
-				<a class="list-group-item<?= $s == $section ? ' active' : '' ?>" href=<?= url('manual.php', ['section' => $s]) ?>><?= $t ?></a>
-
-<?php } ?>
-
+	<!--<div style="height: 20px;"></div>-->
+	<div class="panel panel-default">
+		<div class="panel-body">
+			<div class=row>
+				<div class=col-md-3>
+					<div class=list-group>
+		
+		<?php foreach ($sections as $s => $t) { ?>
+		
+						<a class="list-group-item<?= $s == $section ? ' active' : '' ?>" href=<?= url('manual.php', ['section' => $s]) ?>><?= $t ?></a>
+		
+		<?php } ?>
+		
+					</div>
+				</div>
+				<div class=col-md-9>
+		
+		<?php require('inc/manual/' . $section . '.inc.php'); ?>
+		
+				</div>
 			</div>
-		</div>
-		<div class=col-md-9>
-
-<?php require('inc/manual/' . $section . '.inc.php'); ?>
-
 		</div>
 	</div>
 </div>
