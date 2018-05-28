a231846c28b9c919406976e111d17643c3bb8346
Kilian Evang <noreply@texttheater.net>
2018-05-28 10:49:24 +0200

    Added some main code to tokenization module

--- src/python/tokenization.py
+++ src/python/tokenization.py
@@ -6,6 +6,7 @@
 
 
 import re
+import sys
 import ucto
 
 
@@ -29,3 +30,18 @@ class Tokenizer:
             return ''
             #raise RuntimeError('Nomatch: {} {}'.format(repr(tokens), repr(line)))
         return ' '.join(str(t) for t in tokens)
+
+
+if __name__ == '__main__':
+    try:
+        _, lang = sys.argv
+    except ValueError:
+        print('USAGE: python3 tokenization.py LANG', file=sys.stderr)
+        sys.exit(1)
+    t = Tokenizer(lang)
+    for line in sys.stdin:
+        tokenized = t.tokenize(line)
+        if tokenized:
+            print(t.tokenize(line))
+        else:
+            print()
