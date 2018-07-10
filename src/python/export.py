f444c4c1822e4bd12d21b6cab315618c559f73f6
Kilian Evang <noreply@texttheater.net>
2018-07-10 09:38:58 +0200

    Added annotator parses to export

--- src/python/export.py
+++ src/python/export.py
@@ -108,6 +108,90 @@ def export_train(lang, datadir):
             trg_tok_file.write(trg_tokenized + '\n')
 
 
+def export_user_devtest_trg(lang, user_name, datadir):
+    rows = ccgweb.db.get('''SELECT s1.sentence_id, s1.sentence, s2.sentence_id, s2.sentence, cu1.parse
+                            FROM sentence_links AS l
+                            INNER JOIN sentences AS s1
+                            ON (l.lang1, l.id1) = (s1.lang, s1.sentence_id)
+                            INNER JOIN sentences AS s2
+                            ON (l.lang2, l.id2) = (s2.lang, s2.sentence_id)
+                            INNER JOIN correct AS cj1
+                            ON (s1.lang, s1.sentence_id) = (cj1.lang, cj1.sentence_id)
+                            INNER JOIN correct AS cj2
+                            ON (s2.lang, s2.sentence_id) = (cj2.lang, cj2.sentence_id)
+                            LEFT OUTER JOIN correct AS cu1
+                            ON (s1.lang, s1.sentence_id) = (cu1.lang, cu1.sentence_id)
+                            AND cu1.user_id = %s
+                            WHERE l.lang1 = %s
+                            AND l.lang2 = "eng"
+                            AND s1.assigned = 1
+                            AND s2.assigned = 1
+                            AND cj1.user_id = "judge"
+                            AND cj2.user_id = "judge"''', user_name, lang)
+    # Check that each target sentence only appears once:
+    ids = [r[0] for r in rows]
+    assert len(ids) == len(set(ids))
+    # Split into dev and test:
+    rows = sorted(rows, key=lambda x: x[2]) # sort by English sentence ID
+    sentences = {}
+    sentences['dev'] = rows[:len(rows) // 2]
+    sentences['test'] = rows[len(rows) // 2:]
+    # Export:
+    for portion, portion_sentences in sentences.items():
+        trg_parse_path = os.path.join(datadir, portion + '.' + lang + '-eng.trg.' + user_name + '.parse.tags')
+        with open(trg_parse_path, 'w') as trg_parse_file:
+            for i, (trg_sentence_id, trg_sentence, _, _, trg_parse) in enumerate(portion_sentences, start=1):
+                if trg_parse is None:
+                    continue
+                preamble, trg_parse = trg_parse.split('\n\n', 1)
+                assert is_block(trg_parse)
+                assert trg_parse.startswith('ccg(1,\n')
+                trg_parse = 'ccg(' + str(i) + trg_parse[5:]
+                trg_parse_file.write(trg_parse)
+
+
+def export_user_devtest_src(lang, user_name, datadir):
+    rows = ccgweb.db.get('''SELECT s1.sentence_id, s1.sentence, s2.sentence_id, s2.sentence, cu2.parse
+                            FROM sentence_links AS l
+                            INNER JOIN sentences AS s1
+                            ON (l.lang1, l.id1) = (s1.lang, s1.sentence_id)
+                            INNER JOIN sentences AS s2
+                            ON (l.lang2, l.id2) = (s2.lang, s2.sentence_id)
+                            INNER JOIN correct AS cj1
+                            ON (s1.lang, s1.sentence_id) = (cj1.lang, cj1.sentence_id)
+                            INNER JOIN correct AS cj2
+                            ON (s2.lang, s2.sentence_id) = (cj2.lang, cj2.sentence_id)
+                            LEFT OUTER JOIN correct AS cu2
+                            ON (s2.lang, s2.sentence_id) = (cu2.lang, cu2.sentence_id)
+                            AND cu2.user_id = %s
+                            WHERE l.lang1 = %s
+                            AND l.lang2 = "eng"
+                            AND s1.assigned = 1
+                            AND s2.assigned = 1
+                            AND cj1.user_id = "judge"
+                            AND cj2.user_id = "judge"''', user_name, lang)
+    # Check that each target sentence only appears once:
+    ids = [r[0] for r in rows]
+    assert len(ids) == len(set(ids))
+    # Split into dev and test:
+    rows = sorted(rows, key=lambda x: x[2]) # sort by English sentence ID
+    sentences = {}
+    sentences['dev'] = rows[:len(rows) // 2]
+    sentences['test'] = rows[len(rows) // 2:]
+    # Export:
+    for portion, portion_sentences in sentences.items():
+        src_parse_path = os.path.join(datadir, portion + '.' + lang + '-eng.src.' + user_name + '.parse.tags')
+        with open(src_parse_path, 'w') as src_parse_file:
+            for i, (_, _, src_sentence_id, src_sentence, src_parse) in enumerate(portion_sentences, start=1):
+                if src_parse is None:
+                    continue
+                preamble, src_parse = src_parse.split('\n\n', 1)
+                assert is_block(src_parse)
+                assert src_parse.startswith('ccg(1,\n')
+                src_parse = 'ccg(' + str(i) + src_parse[5:]
+                src_parse_file.write(src_parse)
+
+
 def export_devtest(lang, datadir):
     rows = ccgweb.db.get('''SELECT s1.sentence_id, s1.sentence, c1.parse, s2.sentence_id, s2.sentence, c2.parse
                             FROM sentence_links AS l
@@ -133,6 +217,7 @@ def export_devtest(lang, datadir):
     sentences = {}
     sentences['dev'] = rows[:len(rows) // 2]
     sentences['test'] = rows[len(rows) // 2:]
+    # Create tokenizers:
     src_tokenizer = tokenization.Tokenizer('eng')
     trg_tokenizer = tokenization.Tokenizer(lang)
     # Export:
@@ -180,8 +265,12 @@ if __name__ == '__main__':
     except ValueError:
         print('USAGE: python3 export.py DATADIR', file=sys.stderr)
         sys.exit(1)
-    for lang in ccgweb.supported_languages:
+    for lang in ('deu', 'ita', 'nld'):
         ccgweb.util.makedirs(datadir)
+        for user_name in reversed(ccgweb.config['human_annotators']):
+            export_user_devtest_trg(lang, user_name, datadir)
+            export_user_devtest_src(lang, user_name, datadir)
+        export_devtest(lang, datadir)
+    for lang in ccgweb.supported_languages:
         export_proj1(lang, datadir)
         export_train(lang, datadir)
-        export_devtest(lang, datadir)
